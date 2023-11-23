7c27859d862a6a05d945108a29ec00d347524c9c
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-11-23 20:30:10 -0300

    Começo do meu Mod de otimização

--- src/client/java/com/example/mixin/client/ExampleClientMixin.java
+++ src/client/java/com/example/mixin/client/ExampleClientMixin.java
@@ -1,15 +1,40 @@
 package com.example.mixin.client;
 
-import net.minecraft.client.MinecraftClient;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.BlockPos;
 
-@Mixin(MinecraftClient.class)
-public class ExampleClientMixin {
-	@Inject(at = @At("HEAD"), method = "run")
-	private void run(CallbackInfo info) {
-		// This code is injected into the start of MinecraftClient.run()V
-	}
+public class OptimizeChunkGeneration {
+
+    public static void optimizeChunkGeneration(Minecraft mc) {
+        // Obtém a posição atual do jogador.
+        BlockPos pos = mc.player.getPosition();
+
+        // Cria uma lista de chunks que precisam ser gerados.
+        List<BlockPos> chunksToGenerate = new ArrayList<>();
+
+        // Para cada direção, verifica se o chunk adjacente precisa ser gerado.
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                for (int dz = -1; dz <= 1; dz++) {
+                    if (dx != 0 || dy != 0 || dz != 0) {
+                        BlockPos chunkPos = pos.add(dx, dy, dz);
+                        if (!mc.world.isChunkGenerated(chunkPos)) {
+                            chunksToGenerate.add(chunkPos);
+                        }
+                    }
+                }
+            }
+        }
+
+        // Gera os chunks na lista.
+        for (BlockPos chunkPos : chunksToGenerate) {
+            mc.world.generateChunk(chunkPos);
+        }
+    }
+
+    public static void main(String[] args) {
+        Minecraft mc = Minecraft.getInstance();
+        optimizeChunkGeneration(mc);
+    }
 }
